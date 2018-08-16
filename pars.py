7bbe38b5f6941e64a4603e14a39ec1a5e2f7bc45
ZubnayaFeya <danilal@mail.ru>
2018-08-16 17:26:14 +0300

    add id auction

--- pars.py
+++ pars.py
@@ -4,6 +4,7 @@
 import urllib.request
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 url = 'https://шанти-шанти.рф/shantisales/'
@@ -15,13 +16,16 @@ def get_html(url):
 
 
 def parse(html):
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'lxml')
     table = soup.find('table', class_='table table-striped')
 
     products = []
 
     for row in table.find_all('tr')[1:]:
         cols = row.find_all('td')
+        link = row.find_all('a')[0].get('href')
+        pattern = r'[0-9]+[\_][0-9]+'
+        id = int(re.findall(pattern, link)[0])
         a = cols[1].text
 
         prod = a.split('  ')
@@ -30,6 +34,8 @@ def parse(html):
                 prod.remove('')
 
         products.append({
+            'link_vk': link,  # https://vk.com/wall-12345678_123456
+            'id': id,
             'title': prod[0].strip(),
             'money': prod[1].replace('\xa0', ' ').strip(),
             'name': cols[2].text.strip(),
@@ -37,7 +43,7 @@ def parse(html):
         })
 
     for product in products:
-        print('{} - {} - {} - {}'.format(product['title'], product['money'], product['name'], product['money2']))
+        print('{} - {} - {} - {} - {}'.format(product['id'], product['title'], product['money'], product['name'], product['money2']))
     print(len(products))
 
 def main():
